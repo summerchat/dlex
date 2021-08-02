8b0f8b2ad4482ab6bcee95140b4780de6552da25
Jamil Abreu <abreu.jamil@gmail.com>
2021-08-01 22:53:15 -0400

    update deps

--- mix.exs
+++ mix.exs
@@ -28,15 +28,17 @@ defmodule Dlex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:db_connection, "~> 2.1"},
-      {:grpc, "~> 0.3.1"},
-      {:jason, "~> 1.0", optional: true},
-      {:mint, "~> 1.0", optional: true},
-      {:castore, "~> 0.1.4", optional: true},
-      {:ecto, "~> 3.1", optional: true},
+      {:db_connection, "~> 2.4"},
+      {:grpc, github: "elixir-grpc/grpc"},
+      {:gun, github: "ninenines/gun", override: true},
+      {:cowlib, "~> 2.11", override: true},
+      {:jason, "~> 1.2", optional: true},
+      {:mint, "~> 1.3", optional: true},
+      {:castore, "~> 0.1.11", optional: true},
+      {:ecto, "~> 3.6", optional: true},
       {:earmark, "~> 1.4", only: :dev},
-      {:exrun, "~> 0.1.0", only: :dev},
-      {:ex_doc, "~> 0.19", only: :dev}
+      {:exrun, "~> 0.1.6", only: :dev},
+      {:ex_doc, "~> 0.25", only: :dev}
     ]
   end
 
